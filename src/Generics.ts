075fd93b201088c5157234aa29689b0ca307fbd5
KotenkoNata <kotenkonata8@gmail.com>
2022-12-22 21:39:51 +0200

    ref: delete data

--- src/Generics.ts
+++
@@ -1,7 +0,0 @@
-
-function append<T>(el:T, list:T[]):T[] {
-    return list.concat(el)
-}
-
-append(10, [5,6,7])
-
--- src/Literals.ts
+++
@@ -1,9 +0,0 @@
-type Style = 'none' | 'dotted' | 'dashed' | 'solid';
-
-type Color = 'Red' | 'Green' | 'Blue' | 'Black' | 'White';
-
-type BorderStyle = `${Style} ${Uncapitalize<Color>}`;
-
-let borderStyle: BorderStyle = 'solid red';
-
-let borderStyle2: BorderStyle = 'dashed white';
